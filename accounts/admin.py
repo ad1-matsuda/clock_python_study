3f025800549b2ef42c90820814ba00a8e5ca5d63
lv20koji <84364462+lv20koji@users.noreply.github.com>
2022-06-28 20:05:51 +0900

    webスクレイピング以外のファイルを削除

--- accounts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from attendance.models import SubmitAttendance
-# 作ったモデルを登録する
-admin.site.register(SubmitAttendance)
--- attendance/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import SubmitAttendance
-
-
-class SubmitAttendanceForm(forms.ModelForm):
-
-    class Meta:
-        model = SubmitAttendance
-        fields = ('in_out',)
